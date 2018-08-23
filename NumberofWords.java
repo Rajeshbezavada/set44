0aab3611ad872bc5f4b42c25938665251b1a1f6f
Rajeshbezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-23 00:03:34 -0400

    Create NumberofWords.java

---
+++ NumberofWords.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class NumberofWords
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int count=1;
+		for(int i=0;i<str.length()-1;i++)
+		{
+			if((str.charAt(i)==' ')&&(str.charAt(i+1)!=' '))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
